d4c24b3cb0c794d176950df270b7a9381b949fd9
BznGit <beznyakov@gmail.com>
2024-06-11 13:33:56 +0300

    time

--- backend/controllers/defaultData.js
+++ backend/controllers/defaultData.js
@@ -19,7 +19,7 @@ const defaultData = class {
         let delta = (nw-cur) 
         for (let i = 0; i < this.length - 1; i++){
             let obj = {
-                x: new Date((+start + delta*i)).setHours(hourDelta, 0, 0, 0),
+                x: new Date((+start + delta*i)).setHours(-hourDelta, 0, 0, 0),
                 y: Math.random()*10
             }
             
