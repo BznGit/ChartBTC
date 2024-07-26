867bd8a3ff143ad8c6fb14824dac08b1f64ed1ac
BznGit <bez-al@mail.ru>
2024-07-26 22:33:34 +0300

    127

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -1,12 +1,12 @@
 const cors = require("cors");
 const config = require("../config");
 
-const whitelist = new Set(["https://example1.com", "https://example2.com", `http://127.0.0.1:${config.port}`]);
+const whitelist = new Set(["https://example1.com", "https://example2.com", `http://localhost:${config.port}`]);
 const corsOptions = {
   optionsSuccessStatus: 200,
 
   origin: function(origin, callback) {
-    if (origin === undefined) origin = `http://127.0.0.1:${config.port}`
+    if (origin === undefined) origin = `http://localhost:${config.port}`
     if (whitelist.has(origin)) {  
       callback(null, true);
     } else {
